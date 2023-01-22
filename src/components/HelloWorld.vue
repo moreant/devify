10058d76499d738c4d7ba024d9a4ec9b6281ba47
moreant <moreant@foxmail.com>
2023-01-23 00:30:36 +0800

    test update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,14 +4,14 @@
   defineProps<{ msg: string }>()
 
   const count = ref(0)
-  count.value = 3
+  count.value = 0
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="count += 2">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
