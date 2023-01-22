7287c3d2f142fc9ed2926d57e1a74ed619650ba1
moreant <moreant@foxmail.com>
2023-01-23 01:00:23 +0800

    prompt

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    console.log('onNeedRefresh')
+    const result = window.confirm('onNeedRefresh')
+    if (result) {
+      updateSW()
+    }
+  },
+  onOfflineReady() {
+    console.log('onOfflineReady')
+  }
+})
 
 createApp(App).mount('#app')
