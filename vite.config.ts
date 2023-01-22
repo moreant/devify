f48bf695d2057a6aea865104cbc4523da955998f
moreant <moreant@foxmail.com>
2023-01-23 00:43:48 +0800

    pwa-test

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,30 @@ export default defineConfig({
       imports: ['vue', 'vue-router']
     }),
     Components(),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Devify - tools for web developers',
+        short_name: 'Devify',
+        description: 'tools for web developers',
+        theme_color: '#2563eb',
+        background_color: '#2563eb',
+        display: 'fullscreen',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   server: {
     host: true
